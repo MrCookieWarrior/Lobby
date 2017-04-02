c6163acb7afdff2f5d778e0a9629c1b1f834fcb0
DieSeNse14 <DieSeNse14@DieSeNse14-PC.fritz.box>
2017-04-02 13:07:04 +0200

    edit config.yml

--- Lobby/src/de/mrcookiewarrior/lobby/commands/CMD_Ban.java
+++ Lobby/src/de/mrcookiewarrior/lobby/commands/CMD_Ban.java
@@ -5,6 +5,9 @@
  */
 package de.mrcookiewarrior.lobby.commands;
 
+import de.mrcookiewarrior.lobby.main.Main;
+import de.mrcookiewarrior.lobby.manager.BanManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,6 +20,32 @@ public class CMD_Ban implements CommandExecutor {
     
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("ban")) {
+            if(sender.hasPermission("lobby.ban") || sender.hasPermission("lobby.*")) {
+                if(args.length == 2) {
+                    String playername = args[0];
+                    if(BanManager.isBanned(getUUID(playername))) {
+                        sender.sendMessage(Main.getLobby().Prefix + "§cDieser Spieler ist bereits gebannt!");
+                        return true;
+                    }
+                    String reason = "";
+                    for(int i = 1; i < args.length; i++) {
+                        reason = reason + args[i] + " ";
+                    }
+                    BanManager.ban(getUUID(playername), playername, reason, -1);
+                    sender.sendMessage(Main.getLobby().Prefix + "§7Du hast §e" + playername + " §7erfolgreich vom Server gebannt!");
+                    return true;
+                } else {
+                    sender.sendMessage(Main.getLobby().Prefix + "§3Use: §7/ban <Player> <Reason>");
+                    return true;
+                }
+            } else {
+                sender.sendMessage(Main.getLobby().Prefix + Main.getLobby().NoPermCMD);
+            }
+        }
         return true;
-    }    
+    }
+    private String getUUID(String playername) {
+        return Bukkit.getOfflinePlayer(playername).getUniqueId().toString();
+    }
 }
